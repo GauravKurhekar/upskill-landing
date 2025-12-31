1e002f3a04e466e545b9f5ef29c8677145b393db
mpiyush15 <piyush@pixelsdigital.tech>
2025-12-31 17:02:51 +0530

    Reduce top padding in Hero section to minimize gap below navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -51,7 +51,7 @@ export default function Hero() {
       ))}
 
       {/* Main content */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-0 pb-16 text-center">
         <motion.div
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
